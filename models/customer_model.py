31c02b0cc7c56e29f27c09b67be1b626f6f3765a
Fabricyo Barreto <bass.barreto@gmail.com>
2025-06-05 17:34:04 -0300

    feat(models): add customer model

---
+++ models/customer_model.py
@@ -0,0 +1,26 @@
+class Customer:
+    def __init__(self, id: int, name: str, email: str, phone: str):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone
+        }
+    
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id = data.get("id", 0),
+            name = data.get("name", ""),
+            email = data.get("email", ""),
+            phone = data.get("phone", "")
+        )
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name}, email={self.email}, phone={self.phone})" 
